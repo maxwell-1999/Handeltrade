9bb798f8db4f106ef6124a2510c896aa2bc1a902
maxwell-1999 <maxwell@buffer.finance>
2023-12-20 22:58:23 +0530

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -27,7 +28,13 @@ export default defineConfig(({ command, mode }) => {
     build: {
       target: ['esnext'], // 👈 build.target
     },
-    plugins: [],
+    plugins: [
+      react({
+        babel: {
+          plugins: ['macros'],
+        },
+      }),
+    ],
 
     define: {
       __APP_ENV__: env.APP_ENV,
