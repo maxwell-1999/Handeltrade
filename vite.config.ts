dfc93e9d195030a4fe48d1eaccb3d3fbef5d0b76
maxwell-1999 <maxwell@buffer.finance>
2024-01-08 18:12:06 +0530

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
@@ -32,6 +32,18 @@ export default defineConfig(({ command, mode }) => {
       react({
         babel: {
           plugins: ['macros'],
+        },
+      }),
+      VitePWA({
+        manifest: {
+          icons: [
+            {
+              src: '/AppLogo.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any maskable',
+            },
+          ],
         },
       }),
     ],
