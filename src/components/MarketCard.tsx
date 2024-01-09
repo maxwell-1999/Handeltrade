bedaf80fe2a07b2586ea9c07b50e94cc5a5a250d
maxwell-1999 <maxwell@buffer.finance>
2024-01-09 11:47:18 +0530

    WIP

--- src/components/MarketCard.tsx
+++ src/components/MarketCard.tsx
@@ -31,22 +31,29 @@ const MarketCard: React.FC<{
       onClick={() => !preview && navigate('/markets/' + market.market_id)}
     >
       <div className="flex flex-col gap-[3px] items-center justify-center ">
-        <img src={market?.img_url} className="w-[40px] h-[40px] rounded-[5px]" />
+        <img
+          src={market?.img_url}
+          className="w-[40px] h-[40px] rounded-[5px]"
+        />
         {nonPrice ? null : (
-          <span className="font-semibold text-f14">#{market?.rank || "New"}</span>
+          <span className="font-semibold text-f14">
+            #{market?.rank || 'New'}
+          </span>
         )}
       </div>
       <div className="flex flex-col items-center w-full ">
         <div
-          className={`flex justify-between w-full mb-[2px] mt-${nonPrice ? '1' : '2'
-            } `}
+          className={`flex justify-between w-full mb-[2px] mt-${
+            nonPrice ? '1' : '2'
+          } `}
         >
           <span className="font-semibold text-f14">{market.name}</span>
           {nonPrice ? null : (
             <span
               data-tooltip-id="tooltip"
-              data-tooltip-content={"Total shares supply of the market"}
-              className="font-[500] text-f10 text-2 cursor-pointer">
+              data-tooltip-content={'Total shares supply of the market'}
+              className="font-[500] text-f10 text-2 cursor-pointer"
+            >
               {view(market?.shares)} Shares
             </span>
           )}{' '}
@@ -67,8 +74,10 @@ const MarketCard: React.FC<{
                 <TimeAgo date={toJSEpoch(market.lastUpdated)} />
                 <div>
                   Price{' '}
-                  {market?.buyPrice ? viewDec(BigInt(market.buyPrice)) : 'No Price'}{' '}
-                  {network.chain?.nativeCurrency.symbol}
+                  {market?.buyPrice
+                    ? viewDec(BigInt(market.buyPrice))
+                    : 'No Price'}{' '}
+                  {'ETH'}
                 </div>
               </div>
               {preview ? null : (
