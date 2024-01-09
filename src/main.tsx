063499ec6cc73fefe8e51d3d4c24f6dac9007fdd
maxwell-1999 <maxwell@buffer.finance>
2024-01-09 13:50:00 +0530

    WIP

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <div className="grid place-items-center">
+          <h1>Something went wrong</h1>.
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
+);
