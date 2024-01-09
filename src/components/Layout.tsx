f176fb54212261e300c75142b66a3d550c9f9384
maxwell-1999 <maxwell@buffer.finance>
2024-01-09 13:26:34 +0530

    WIP

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -31,52 +31,55 @@ const isNestedRouteActive = (page: string) => {
     return true;
   return false;
 };
-const Layout: React.FC<{ children: React.ReactNode; }> = ({ children }) => {
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [activeTab, setActiveTab] = useState(Icons[0].name);
   const navigate = useNavigate();
   const drawerManager = useDrawerState();
-  return (<>
-    <div className="flex flex-shrink-0 h-[100vh] align-middle items-center justify-center ">
-      <div className="flex flex-col responsive-layout  h-[100vh] ">
-        <div className="w-full bg-[#eaebf0] h-[40px] items-center justify-between flex px-[10px]">
-          <img className="w-[35px] h-[25px] " src="Logo.svg" />
-          <AccountDropdown />
-        </div>
-        <div className="flex-1 w-full container-height-fr">{children}</div>
-        {drawerManager.drawerState?.screen ? <MobileDrawer /> : null}
+  return (
+    <>
+      <div className="flex flex-shrink-0 h-[100vh] align-middle items-center justify-center ">
+        <div className="flex flex-col responsive-layout  h-[100vh] ">
+          <div className="w-full bg-[#eaebf0] h-[40px] items-center justify-between flex px-[10px]">
+            <img className="w-[35px] h-[25px] " src="Logo.svg" />
+            <AccountDropdown />
+          </div>
+          <div className="flex-1 w-full container-height-fr">{children}</div>
+          {drawerManager.drawerState?.screen ? <MobileDrawer /> : null}
 
-        <div className="h-[50px] w-full flex justify-center gap-[40px] items-center  fixed bottom-0 left-0 text-2 ">
-          {Icons.map((icon) => {
-            return (
-              <NavLink to={'/' + icon.page} key={icon.name}>
-                {({ isActive }) => (
-                  <button
-                    key={icon.name}
-                    onClick={(e) => {
-                      navigate('/' + icon.page);
-                    }}
-                    className={
-                      isActive || isNestedRouteActive(icon.page)
-                        ? 'text-brand'
-                        : ''
-                    }
-                  >
-                    {icon.icon}
-                  </button>
-                )}
-              </NavLink>
-            );
-          })}
+          <div className="h-[50px] w-full flex justify-center gap-[20vw] items-center  fixed bottom-0 left-0 text-2 ">
+            {Icons.map((icon) => {
+              return (
+                <NavLink to={'/' + icon.page} key={icon.name}>
+                  {({ isActive }) => (
+                    <button
+                      key={icon.name}
+                      onClick={(e) => {
+                        navigate('/' + icon.page);
+                      }}
+                      className={
+                        isActive || isNestedRouteActive(icon.page)
+                          ? 'text-brand'
+                          : ''
+                      }
+                    >
+                      {icon.icon}
+                    </button>
+                  )}
+                </NavLink>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
-    <Tooltip
-      delayShow={1500}
-      style={{
-        backgroundColor: '#E5E7EB',
-        color: "#8F95A4",
-      }} id="tooltip" />
-  </>
+      <Tooltip
+        delayShow={1500}
+        style={{
+          backgroundColor: '#E5E7EB',
+          color: '#8F95A4',
+        }}
+        id="tooltip"
+      />
+    </>
   );
 };
 
