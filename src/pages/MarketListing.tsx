28a1a3fe221beabeb9fdf681097c939ca8e1e71d
maxwell-1999 <maxwell@buffer.finance>
2024-01-22 11:50:11 +0530

    WIP

--- src/pages/MarketListing.tsx
+++ src/pages/MarketListing.tsx
@@ -84,7 +84,7 @@ const New = () => {
     },
     refreshInterval: marketsRefreshInterval,
   });
-  return <ListLoader />;
+  // return <ListLoader />;
   console.log(`MarketListing-data: `, data);
   if (!data?.length) return <ListLoader />;
   return <MarketList markets={data} />;
